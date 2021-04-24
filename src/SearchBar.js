0ed1b3fa0fe6cb544963703b919ec210a8de221f
RGZhouYC <RGZhouYC@users.noreply.github.com>
2021-04-23 23:30:36 -0400

    Add files via upload

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,30 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from './Button'
 //import { NativeRouter, Route, Link } from "react-router-native";
 import { Link } from 'react-router-dom'
 import SearchResult from './SearchResult'
 
-const SearchBar = ({ keyword, setKeyword }) => {
-  const onClick = () => {
-    console.log('click')
-  }
+export function SearchBar(props) {
+  const [name, setName] = useState("");
 
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    alert(`Searching ${name}`)
+  }
   return (
     <div>
-      <input
-        className='searchbar'
-        key="random1"
-        value={keyword}
-        placeholder={"search item"}
-        onChange={(e) => setKeyword(e.target.value)} />
-      <Link to='/SearchResult'>
-        <Button
-          color={'green'}
-          text={'Go'}
-          onClick={onClick} />
-      </Link>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          placeholder={"search item"}
+          className='searchbar'
+          onChange={e => setName(e.target.value)}
+        />
+        <Link to='/SearchResult'>
+          <input type="submit" value="Go" className='btn'/>
+        </Link>
+      </form>
     </div>
   );
 }
+
+// const SearchBar = ({ keyword, setKeyword }) => {
+//   const onClick = () => {
+//     console.log('click')
+//   }
+
+//   return (
+//     <div>
+//       <input
+//         className='searchbar'
+//         key="random1"
+//         value={keyword}
+//         placeholder={"search item"}
+//         onChange={(e) => setKeyword(e.target.value)} />
+//       <Link to='/SearchResult'>
+//         <Button
+//           color={'green'}
+//           text={'Go'}
+//           onClick={onClick} />
+//       </Link>
+//     </div>
+//   );
+// }
 
 export default SearchBar
